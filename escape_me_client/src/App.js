c5b7c339807e60b5ecdcd4bc7229f18ff2e0e056
Eero Vilpponen <eero.vilpponen@gmail.com>
2018-06-04 10:30:26 +0300

    Initial working version of socket.io events

--- escape_me_client/src/App.js
+++ escape_me_client/src/App.js
@@ -1,22 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import socketIOClient from 'socket.io-client'
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    
+    this.state = {
+      endpoint: "http://localhost:5000" // this is where we are connecting to with sockets
+    }
+    this.socket = socketIOClient(this.state.endpoint)
+  }
+  
+  // method for emitting a socket.io event
+  send = () => {
+    // this emits an event to the socket (your server) with an argument of 'red'
+    // you can make the argument any color you would like, or any kind of data you want to send.
+    
+    console.log("sending")
+    this.socket.emit('my_message', {data: 'react message'}) 
+    // socket.emit('change color', 'red', 'yellow') | you can have multiple arguments
+  }
+  
+  // render method that renders in code if the state is updated
   render() {
+    
+    // socket.on is another method that checks for incoming events from the server
+    // This method is looking for the event 'change color'
+    // socket.on takes a callback function for the first argument
+    this.socket.on('my_response', (message) => {
+      // setting the color of our button
+      console.log(message)
+    })
+    this.socket.on('connect', (message) => {
+      // setting the color of our button
+      this.socket.emit('my_event', {data: 'Host connected!'});
+      console.log(message)
+    })
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div style={{ textAlign: "center" }}>
+        <button onClick={() => this.send()}>Change Color</button>
       </div>
-    );
+    )
   }
 }
-
 export default App;
