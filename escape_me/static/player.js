3011e1ff7caf59d4a093dc26e6cb8ecc5f3d7c15
Eero Vilpponen <eero.vilpponen@gmail.com>
2018-06-06 10:07:59 +0300

    Fix problems with player hint states

--- escape_me/static/player.js
+++ escape_me/static/player.js
@@ -1,33 +1,24 @@
+var hint_body = "";
+
 $(document).ready(function() {
 
     // Connect to the Socket.IO server.
     var socket = io.connect();
-    var message = "";
-
-    text_animate_loop = null;
-    function text_animate() {
-        var new_char = message.charAt($('#msg').text().length)
-        $('#msg').text($('#msg').text() + new_char)
-        if (message.length == $('#msg').text().length) {
-            console.log("Done animating!");
-            clearInterval(text_animate_loop);
-            $('#request_btn').removeAttr('disabled');
-        }
-    }
+    var initial = true;
 
     socket.on('player_ping', function() {
         socket.emit('player_pong');
     });
     socket.on('hint_send', function(msg) {
-        $('#msg').text("")
-        message = msg['hint_body'];
-        clearInterval(text_animate_loop);
-        text_animate_loop = setInterval(text_animate, 100);
-        animate_hint_requested(false);
+        set_hint(msg['hint_body']);
     });
     socket.on('database', function(data) {
-        console.log("database: " + data)
+        console.log(data)
         animate_hint_requested(data.state.hint_requested);
+        if (initial) {
+            initial = false;
+            set_hint(data.state.hint_body, no_animation = true);
+        }
     });
 
     $('#request_btn').on('click', function(event) {
@@ -35,6 +26,29 @@ $(document).ready(function() {
         console.log("hint_request")
     });
 });
+
+function set_hint(new_hint_body, no_animation = false) {
+    $('#msg').text("")
+    hint_body = new_hint_body;
+    clearInterval(text_animate_loop);
+    if (!no_animation) {
+        text_animate_loop = setInterval(text_animate, 100);
+    } else {
+        $('#msg').text(new_hint_body);
+    }
+    animate_hint_requested(false);
+}
+
+text_animate_loop = null;
+function text_animate() {
+    var new_char = hint_body.charAt($('#msg').text().length)
+    $('#msg').text($('#msg').text() + new_char)
+    if (hint_body.length == $('#msg').text().length) {
+        console.log("Done animating!");
+        clearInterval(text_animate_loop);
+        $('#request_btn').removeAttr('disabled');
+    }
+}
 
 hint_requested_loop = null;
 function animate_hint_requested(start_stop) {
